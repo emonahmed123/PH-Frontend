86db9b264f1f11a51c44fda8a7500431e521d29e
Emon Ahmed <imonshomon@gmail.com>
2024-10-26 21:56:52 +0600

    done daynamic side bar

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,41 @@
+import Sider from "antd/es/layout/Sider";
+import { sidebarItemsGenerator } from "../../utils/sidebarGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+import { Menu } from "antd";
+
+const Sidebar = () => {
+  return (
+    <Sider
+      theme="dark"
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div
+        style={{
+          color: "white",
+
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>PH Uni</h1>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItemsGenerator(adminPaths, "admin")}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
